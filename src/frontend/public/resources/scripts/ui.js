d47446d991c0c77dd7e7587f63225aee6b003057
Anders Busch <andersbusch@gmail.com>
2016-06-25 13:51:19 +0200

    fine java script

--- src/frontend/public/resources/scripts/ui.js
+++ src/frontend/public/resources/scripts/ui.js
@@ -1,41 +1,42 @@
 $(function() {
 	var editor = ace.edit("editor");
     var result = ace.edit("output");
+    var runButton = $('.runbutton');
+    var exitButton = $('.exitbutton');
+    var snippets = $('#snippets');
+ 
+	runButton.click( function() {
 
-	$('.runbutton').click(function() {
-
-        var url = "/runCode";
+        var request = { url: "/runCode", data: { short: false, program: editor.getValue() } };
 
         result.selectAll();
         result.insert("Running code...");
 
-        $.post({
-        	url: url,
-        	data: { short: false, program: editor.getValue() }
-        }).done(function(data) {
+        $.post(request).done( function(data) {
             result.selectAll();
             result.insert(data);
-        }).fail(function() {
+        }).fail( function() {
             result.selectAll();
             result.insert("ERROR: Connection failed.");
         });
     });
 
-    var snippets = $('#snippets');
- 
-    snippets.change( function() {
-        var url = "resources/snippets/" + snippets.val();
-        $.get(url).done(function(data) { 
-            editor.selectAll();
-            editor.insert(data);
-        }).fail(function(){
-            console.log("Snippet loading failed...");
-        });
-    });
-
-    $('.exitbutton').click( function() {
+    exitButton.click( function() {
+        
         result.selectAll();
         result.insert("Shutting down. Please wait...");
         window.location.href = "/shutdown";
+    });
+
+    snippets.change( function() {
+
+        var url = "resources/snippets/" + snippets.val();
+        
+        $.post(url).done( function(data) { 
+            editor.selectAll();
+            editor.insert(data);
+        }).fail( function() {
+            console.log("Snippet loading failed...");
+        });
     });
 });
